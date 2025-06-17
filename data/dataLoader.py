f0d80cf2bb98ef499f8a40a4b9d51e873df07b1c
lxc0205 <1582842050@qq.com>
2025-06-17 12:44:26 +0800

    edit transform.

--- data/dataLoader.py
+++ data/dataLoader.py
@@ -53,6 +53,7 @@ class DataLoader(object):
         self.batch_size = batch_size
 
         transforms = torchvision.transforms.Compose([
+            torchvision.transforms.Resize((224, 224)),
             torchvision.transforms.ToTensor(),
             torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
         ]) 
